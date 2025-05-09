31cfd000328f3f6b3a40362e8e09610b71280b78
Lucas <160598006+Lulucaxz@users.noreply.github.com>
2025-05-09 17:14:58 -0300

    Delete js directory

--- js/header.js
+++
@@ -1,8 +0,0 @@
-const menuu = document.getElementById("menu-icone");
-const links = document.getElementById("header-links")
-const footerConteudo = document.getElementById("footer-conteudo")
-
-menuu.addEventListener("click", () => {
-    links.classList.toggle("ativo")
-    footerConteudo.classList.toggle("ativo")
-})
