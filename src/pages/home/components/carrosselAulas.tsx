3d91e60a69d3a7d2e815601bc6d284736888990c
Marcos Rocha <dev.marcost.rocha@gmail.com>
2025-06-23 21:02:24 -0300

    Ajustes02

--- src/pages/home/components/carrosselAulas.tsx
+++ src/pages/home/components/carrosselAulas.tsx
@@ -8,6 +8,16 @@ function CarrosselAulas() {
         <hr />
       </div>
 
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/ID_DO_VIDEO"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+
       <div className="container-carrossel">
         <div className="carrosel-card">
           <div
